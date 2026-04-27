83fa12ae8650a84d891fd2b25e3868f565cfbd90
aomSimform <aom.kapadia@simformsolutions.com>
2026-04-27 20:27:09 +0530

    added urls for profile

--- users/urls.py
+++ users/urls.py
@@ -3,3 +3,10 @@ from .views import registerViewSet,LoginViewSet, ProfileView
 
 from rest_framework_simplejwt.views import TokenBlacklistView
 
+
+urlpatterns = [
+    path('register/',registerViewSet.as_view(),name = 'register'),
+    path('login/',LoginViewSet.as_view(),name='login'),
+    path('logout/', TokenBlacklistView.as_view(), name='token_blacklist'),
+    path('profile/', ProfileView.as_view(), name='profile'),
+]
