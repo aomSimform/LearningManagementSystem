2d748b94163aed20f6881c18ea9e49c5a729e487
Jerry Mouse <jerry@tutorialspoint.com>
2026-04-28 19:51:55 +0530

    update submission urls

--- submissions/urls.py
+++ submissions/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path, include 
-from .views import submissionListCreate
+from django.urls import path
+from .views import GradingViewset
 
-
-urlpatterns=[
-    path('',submissionListCreate.as_view())
+urlpatterns = [
+    # List grades for a subsection
+    # GET /api/grades/course/1/subsection/5/
+    path(
+        "course/<int:course>/subsection/<int:subsection>/",
+        GradingViewset.as_view(),
+        name="grading-list-create",
+    ),
 ]
