921724abc31abd7a9474cac1b78fe8305b195511
aomSimform <aom.kapadia@simformsolutions.com>
2026-04-28 19:55:21 +0530

    Replace grading view sets urls

--- submissions/urls.py
+++ submissions/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import GradingViewset
+from .views import submissionListCreate
 
 urlpatterns = [
-    # List grades for a subsection
-    # GET /api/grades/course/1/subsection/5/
+
     path(
         "course/<int:course>/subsection/<int:subsection>/",
-        GradingViewset.as_view(),
-        name="grading-list-create",
+        submissionListCreate.as_view(),
+        name="submissions",
     ),
 ]
